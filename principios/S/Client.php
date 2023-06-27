697b74ad70b3cfbbc19aae64cffdbaf7e81566f7
Bruno Alves <bruno@isoftsolutions.com.br>
2023-06-26 21:48:55 -0300

    feat: S : principio da responsabilidade única

---
+++ principios/S/Client.php
@@ -0,0 +1,33 @@
+<?php
+interface data{
+    public function pack();
+    public function getAllData();
+}
+class Client implements data{
+
+    private $id;
+    private $name;
+    private $email;
+    private $pwd;
+    public function __construct($id,$name,$email,$pwd){
+
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->pwd = $pwd;
+
+    }
+
+    public function pack(){
+        return array(
+            "id" => $this->id,
+            "name" => $this->name,
+            "email" => $this->email,
+            "pwd" => $this->pwd
+        );
+    }
+
+    public function getAllData(){
+        return json_encode($this->pack());
+    }
+}
